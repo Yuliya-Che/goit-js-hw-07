e66d865f7caf935baf1f1ffc393850d3373c4afb
Yuliya Chukhil <yuliya.chukhil@gmail.com>
2020-11-17 23:46:49 +0200

    Правки в комментарии

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,11 @@
-// Напиши скрипт для создания галлереи изображений по массиву данных.
-// В HTML есть список ul#gallery.
-// <ul id="gallery"></ul>
-// Используй массив объектов images для создания тегов img вложенных в li.
-// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
-// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+/*Напиши скрипт для создания галлереи изображений по массиву данных.
+В HTML есть список ul#gallery.
+<ul id="gallery"></ul>
+Используй массив объектов images для создания тегов img вложенных в li.
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+*/
 const images = [
   {
     url:
@@ -31,6 +32,6 @@ pictureGallery.style.justifyContent = "space-between";
 pictureGallery.style.border = "5px dashed teal";
 
 images.map((elem) => {
-  const imgElem = `<li><img src="${elem.url}" alt="${elem.alt}" width="750"></li>`;
+  const imgElem = `<li><img src="${elem.url}" alt="${elem.alt}" width="350"></li>`;
   pictureGallery.insertAdjacentHTML("beforeend", imgElem);
 });
