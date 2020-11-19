43e940e853b9fd43830a89edaa78e89a6eab0f41
Yuliya Chukhil <yuliya.chukhil@gmail.com>
2020-11-20 00:53:46 +0200

    Выносит комментарии за пределы js-файлов (в README.md)

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,3 @@
-// В HTML есть пустой список ul#ingredients.
-/* <ul id="ingredients"></ul> */
-// В JS есть массив строк.
 const ingredients = [
   "Картошка",
   "Грибы",
@@ -9,18 +6,13 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
-// Напиши скрипт, который для каждого элемента массива ingredients создаст
-// отдельный li, после чего вставит все li за одну операцию в список
-// ul.ingredients.Для создания DOM - узлов используй document.createElement().;
 
 const ingredientsUl = document.getElementById("ingredients");
 
 const ingredientsLi = ingredients.map((elem) => {
   const ingredient = document.createElement("li");
   ingredient.textContent = elem;
-  // console.log(ingredient);
   return ingredient;
 });
 
 ingredientsUl.append(...ingredientsLi);
-// console.log(ingredientsUl);
